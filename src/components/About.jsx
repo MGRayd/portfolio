c7e1947868a8a1568547555ad36a27e5f6e2cd7d
MGRayd <mark.j.g@gmx.com>
2025-04-29 12:35:30 +0100

    About update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,8 +3,8 @@ function About() {
       <section className="p-6 max-w-3xl mx-auto">
         <h2 className="text-2xl font-bold mb-2">About Me</h2>
         <p className="text-gray-300">
-        I’m an Analyst Developer at PACT Solutions, specializing in data integration, automation, and cloud-based solutions within higher education. 
-        I work across a variety of platforms and technologies, including student record systems, cloud services like Azure and Google Cloud, and CI/CD pipelines using GitHub. 
+        I’m an Analyst Developer at PACT Solutions, specialising in data integration, automation, and cloud-based solutions within higher education. 
+        I work across a variety of platforms and technologies, including student record systems (SITS), cloud services like Azure and Google Cloud, and CI/CD pipelines using GitHub. 
         My focus is on improving operational workflows, ensuring data integrity, and collaborating with cross-functional teams to deliver robust and scalable solutions.
         </p>
 
